a4c8afa4939dfa7252a7b0d5aea9a273ffd8d334
chao.sun <chao.sun4@pactera.com>
2021-03-26 11:45:49 +0800

    布局调整

--- SwiftDemo/CollectionViewController.swift
+++ SwiftDemo/CollectionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CollectionViewController.swift
 //  SwiftDemo
 //
 //  Created by sunchao on 2021/3/10.
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CollectionViewController: UIViewController {
 
     let lineSpacing: CGFloat = 1.0
     let lnteritemSpacing: CGFloat = 1.0
     let columns: Int = 3
     let titleList = ["日付", "名前", "記事"]
+    let dataList = ["03/26", "孫", "家で仕事家"]
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -32,10 +33,10 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UICollectionViewDataSource {
+extension CollectionViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return self.titleList.count + self.dataList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -46,18 +47,20 @@ extension ViewController: UICollectionViewDataSource {
             switch index / self.columns {
             case 0:
                 label.text = self.titleList[index]
+                cell.contentView.backgroundColor = .systemGray6
             default:
-                label.text = String(indexPath.item + 1)
+                label.text = self.dataList[index - self.columns]
+                cell.contentView.backgroundColor = .white
             }
         }
         return cell
     }
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout {
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let screenWidth = Int(UIScreen.main.bounds.width)
+        let screenWidth = Int(UIScreen.main.bounds.width - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right)
         let spacingCount = self.columns - 1
         let spacingWidth = Int(self.lineSpacing)
         let remaining = CGFloat(screenWidth - spacingCount * spacingWidth)
@@ -67,9 +70,9 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
         case 0:
             return CGSize(width: remaining * 0.5 , height: 50)
         case 1:
-            return CGSize(width: remaining * 0.2 , height: 50)
+            return CGSize(width: remaining * 0.25 , height: 50)
         case 2:
-            return CGSize(width: remaining * 0.3 , height: 50)
+            return CGSize(width: remaining * 0.25 , height: 50)
         default:
             return .zero
         }
@@ -82,9 +85,21 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return self.lnteritemSpacing
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return .zero
+    }
 }
 
-private extension ViewController {
+private extension CollectionViewController {
     
     func loadUserView() {
         // nothing to do
